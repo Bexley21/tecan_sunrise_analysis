773ebf1776ad993dbd653119d83ff26ce498c7f3
Katie <66315386+Bexley21@users.noreply.github.com>
2023-02-07 13:05:59 +0000

    Add files via upload

---
+++ src/functions/growth_comparison_plots.R
@@ -0,0 +1,27 @@
+##function takes a list of provided conditions, and returns growth curves of each on same graph - a subset graph is provided showing just initial region
+
+growth_comaprison_plots <- function(input_df,sample_names, sample_labels, save_location){
+  
+  comparison_graph <- input_df%>%
+    pivot_longer(!time,names_to = "sample", values_to = "OD")%>%
+    mutate(sample = str_sub(sample,1,-3))%>%
+    group_by(time,sample)%>%
+    summarise(OD = median(OD))%>%
+    filter(sample %in% sample_names)%>%
+    ggplot(aes(x=time,y=OD,color=sample)) +
+    scale_color_discrete(
+      limits = sample_names,
+      labels = sample_labels
+    ) +
+   # geom_point()+
+    geom_line()+
+    ggtitle("Analysis of Growth")+
+    theme(plot.title = element_text(hjust = 0.5, face = "bold"))+
+    labs(x= "time (min)", y = "OD")
+  
+  lag_graph <- comparison_graph + ylim(0,0.5) + xlim(0,750) + ggtitle("Duration of lag")
+  
+  ggarrange(comparison_graph, lag_graph, heights = c(2, 1), ncol = 1, nrow = 2)%>%
+    ggsave(save_location,.,)
+  
+}
